58b7b6f9db99cc7a718860cd96025b882a2dd1a9
Mohammed <mohammed.abdelnaby@georgebrown.ca>
2021-11-11 02:33:22 -0500

    final

---
+++ Assets/Lab6PhysicsObjects.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lab6Physics : MonoBehaviour
+{
+    public float mass = 1.0f;
+    public Vector3 velocity = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = new Vector3(0, Mathf.Sin(Time.time),0);
+        transform.position = transform.position + velocity * Time.deltaTime;
+    }
+}
---
+++ Assets/Lab6PhysicsSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lab6PhysicsSystem : MonoBehaviour
+{
+    public Vector3 gravity = new Vector3(0, -9.81f, 0);
+    public List<Lab6Physics> lab6Physics = new List<Lab6Physics>(); 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < lab6Physics.Count; i++)
+        {
+            lab6Physics[i].velocity += gravity * Time.deltaTime;
+        }
+    }
+}
